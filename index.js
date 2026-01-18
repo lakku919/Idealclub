4bb7fbaa2a2d0f5874bcf6476cf637f3d88c4a9b
lakku919 <lakku919@gmail.com>
2026-01-18 19:43:35 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div style={{
+      minHeight: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontFamily: "Arial"
+    }}>
+      <h1>Ideal Club Website Live 🚀</h1>
+    </div>
+  );
+}
